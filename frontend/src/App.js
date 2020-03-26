437ba39c5bd5eecc2726bf8f600e16598b3aef57
Felipe Monteiro <luizfelipe.p@live.com>
2020-03-26 16:20:53 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import Routs from './routes'
+
+import './global.css'
+
+// JSX (Java Script XML)
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routs />
   );
 }
 
-export default App;
+export default App
